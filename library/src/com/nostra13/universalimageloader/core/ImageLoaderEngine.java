b61fe21ca5f5f54942d5bceb794a2ef3a92799bb
Klemen Kresnik <klemen.kresnik@me.com>
2014-04-23 11:39:08 +0200

    Updated ImageServeUtil, added submit method to ImageLoader engine for multipart task.

--- library/src/com/nostra13/universalimageloader/core/ImageLoaderEngine.java
+++ library/src/com/nostra13/universalimageloader/core/ImageLoaderEngine.java
@@ -37,181 +37,237 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author Sergey Tarasevich (nostra13[at]gmail[dot]com)
  * @since 1.7.1
  */
-class ImageLoaderEngine {
+class ImageLoaderEngine
+{
 
-	final ImageLoaderConfiguration configuration;
+    final ImageLoaderConfiguration configuration;
 
-	private Executor taskExecutor;
-	private Executor taskExecutorForCachedImages;
-	private Executor taskDistributor;
+    private Executor taskExecutor;
 
-	private final Map<Integer, String> cacheKeysForImageAwares = Collections
-			.synchronizedMap(new HashMap<Integer, String>());
-	private final Map<String, ReentrantLock> uriLocks = new WeakHashMap<String, ReentrantLock>();
+    private Executor taskExecutorForCachedImages;
 
-	private final AtomicBoolean paused = new AtomicBoolean(false);
-	private final AtomicBoolean networkDenied = new AtomicBoolean(false);
-	private final AtomicBoolean slowNetwork = new AtomicBoolean(false);
+    private Executor taskDistributor;
 
-	private final Object pauseLock = new Object();
+    private final Map<Integer, String> cacheKeysForImageAwares = Collections
+            .synchronizedMap(new HashMap<Integer, String>());
 
-	ImageLoaderEngine(ImageLoaderConfiguration configuration) {
-		this.configuration = configuration;
+    private final Map<String, ReentrantLock> uriLocks = new WeakHashMap<String, ReentrantLock>();
 
-		taskExecutor = configuration.taskExecutor;
-		taskExecutorForCachedImages = configuration.taskExecutorForCachedImages;
+    private final AtomicBoolean paused = new AtomicBoolean(false);
 
-		taskDistributor = DefaultConfigurationFactory.createTaskDistributor();
-	}
+    private final AtomicBoolean networkDenied = new AtomicBoolean(false);
 
-	/** Submits task to execution pool */
-	void submit(final LoadAndDisplayImageTask task) {
-		taskDistributor.execute(new Runnable() {
-			@Override
-			public void run() {
-				File image = configuration.diskCache.get(task.getLoadingUri());
-				boolean isImageCachedOnDisk = image != null && image.exists();
-				initExecutorsIfNeed();
-				if (isImageCachedOnDisk) {
-					taskExecutorForCachedImages.execute(task);
-				} else {
-					taskExecutor.execute(task);
-				}
-			}
-		});
-	}
+    private final AtomicBoolean slowNetwork = new AtomicBoolean(false);
 
-	/** Submits task to execution pool */
-	void submit(ProcessAndDisplayImageTask task) {
-		initExecutorsIfNeed();
-		taskExecutorForCachedImages.execute(task);
-	}
+    private final Object pauseLock = new Object();
 
-	private void initExecutorsIfNeed() {
-		if (!configuration.customExecutor && ((ExecutorService) taskExecutor).isShutdown()) {
-			taskExecutor = createTaskExecutor();
-		}
-		if (!configuration.customExecutorForCachedImages && ((ExecutorService) taskExecutorForCachedImages)
-				.isShutdown()) {
-			taskExecutorForCachedImages = createTaskExecutor();
-		}
-	}
+    ImageLoaderEngine(ImageLoaderConfiguration configuration)
+    {
+        this.configuration = configuration;
 
-	private Executor createTaskExecutor() {
-		return DefaultConfigurationFactory
-				.createExecutor(configuration.threadPoolSize, configuration.threadPriority,
-				configuration.tasksProcessingType);
-	}
+        taskExecutor = configuration.taskExecutor;
+        taskExecutorForCachedImages = configuration.taskExecutorForCachedImages;
 
-	/**
-	 * Returns URI of image which is loading at this moment into passed {@link com.nostra13.universalimageloader.core.imageaware.ImageAware}
-	 */
-	String getLoadingUriForView(ImageAware imageAware) {
-		return cacheKeysForImageAwares.get(imageAware.getId());
-	}
+        taskDistributor = DefaultConfigurationFactory.createTaskDistributor();
+    }
 
-	/**
-	 * Associates <b>memoryCacheKey</b> with <b>imageAware</b>. Then it helps to define image URI is loaded into View at
-	 * exact moment.
-	 */
-	void prepareDisplayTaskFor(ImageAware imageAware, String memoryCacheKey) {
-		cacheKeysForImageAwares.put(imageAware.getId(), memoryCacheKey);
-	}
+    /**
+     * Submits task to execution pool
+     */
+    void submit(final LoadAndDisplayImageTask task)
+    {
+        taskDistributor.execute(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                File image = configuration.diskCache.get(task.getLoadingUri());
+                boolean isImageCachedOnDisk = image != null && image.exists();
+                initExecutorsIfNeed();
+                if (isImageCachedOnDisk)
+                {
+                    taskExecutorForCachedImages.execute(task);
+                }
+                else
+                {
+                    taskExecutor.execute(task);
+                }
+            }
+        });
+    }
 
-	/**
-	 * Cancels the task of loading and displaying image for incoming <b>imageAware</b>.
-	 *
-	 * @param imageAware {@link com.nostra13.universalimageloader.core.imageaware.ImageAware} for which display task
-	 *                   will be cancelled
-	 */
-	void cancelDisplayTaskFor(ImageAware imageAware) {
-		cacheKeysForImageAwares.remove(imageAware.getId());
-	}
+    /**
+     * Submits task to execution pool
+     */
+    void submit(ProcessAndDisplayImageTask task)
+    {
+        initExecutorsIfNeed();
+        taskExecutorForCachedImages.execute(task);
+    }
 
-	/**
-	 * Denies or allows engine to download images from the network.<br /> <br /> If downloads are denied and if image
-	 * isn't cached then {@link ImageLoadingListener#onLoadingFailed(String, View, FailReason)} callback will be fired
-	 * with {@link FailReason.FailType#NETWORK_DENIED}
-	 *
-	 * @param denyNetworkDownloads pass <b>true</b> - to deny engine to download images from the network; <b>false</b> -
-	 *                             to allow engine to download images from network.
-	 */
-	void denyNetworkDownloads(boolean denyNetworkDownloads) {
-		networkDenied.set(denyNetworkDownloads);
-	}
+    void submit(final LoadAndDisplayMultiImageTask task)
+    {
+        taskDistributor.execute(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                initExecutorsIfNeed();
+                taskExecutor.execute(task);
+            }
+        });
+    }
 
-	/**
-	 * Sets option whether ImageLoader will use {@link FlushedInputStream} for network downloads to handle <a
-	 * href="http://code.google.com/p/android/issues/detail?id=6066">this known problem</a> or not.
-	 *
-	 * @param handleSlowNetwork pass <b>true</b> - to use {@link FlushedInputStream} for network downloads; <b>false</b>
-	 *                          - otherwise.
-	 */
-	void handleSlowNetwork(boolean handleSlowNetwork) {
-		slowNetwork.set(handleSlowNetwork);
-	}
 
-	/**
-	 * Pauses engine. All new "load&display" tasks won't be executed until ImageLoader is {@link #resume() resumed}.<br
-	 * /> Already running tasks are not paused.
-	 */
-	void pause() {
-		paused.set(true);
-	}
+    private void initExecutorsIfNeed()
+    {
+        if (!configuration.customExecutor && ((ExecutorService) taskExecutor).isShutdown())
+        {
+            taskExecutor = createTaskExecutor();
+        }
+        if (!configuration.customExecutorForCachedImages && ((ExecutorService) taskExecutorForCachedImages)
+                .isShutdown())
+        {
+            taskExecutorForCachedImages = createTaskExecutor();
+        }
+    }
 
-	/** Resumes engine work. Paused "load&display" tasks will continue its work. */
-	void resume() {
-		paused.set(false);
-		synchronized (pauseLock) {
-			pauseLock.notifyAll();
-		}
-	}
+    private Executor createTaskExecutor()
+    {
+        return DefaultConfigurationFactory
+                .createExecutor(configuration.threadPoolSize, configuration.threadPriority,
+                        configuration.tasksProcessingType);
+    }
 
-	/**
-	 * Stops engine, cancels all running and scheduled display image tasks. Clears internal data.
-	 * <br />
-	 * <b>NOTE:</b> This method doesn't shutdown
-	 * {@linkplain com.nostra13.universalimageloader.core.ImageLoaderConfiguration.Builder#taskExecutor(java.util.concurrent.Executor)
-	 * custom task executors} if you set them.
-	 */
-	void stop() {
-		if (!configuration.customExecutor) {
-			((ExecutorService) taskExecutor).shutdownNow();
-		}
-		if (!configuration.customExecutorForCachedImages) {
-			((ExecutorService) taskExecutorForCachedImages).shutdownNow();
-		}
+    /**
+     * Returns URI of image which is loading at this moment into passed {@link com.nostra13.universalimageloader.core.imageaware.ImageAware}
+     */
+    String getLoadingUriForView(ImageAware imageAware)
+    {
+        return cacheKeysForImageAwares.get(imageAware.getId());
+    }
 
-		cacheKeysForImageAwares.clear();
-		uriLocks.clear();
-	}
+    /**
+     * Associates <b>memoryCacheKey</b> with <b>imageAware</b>. Then it helps to define image URI is loaded into View at
+     * exact moment.
+     */
+    void prepareDisplayTaskFor(ImageAware imageAware, String memoryCacheKey)
+    {
+        cacheKeysForImageAwares.put(imageAware.getId(), memoryCacheKey);
+    }
 
-	void fireCallback(Runnable r) {
-		taskDistributor.execute(r);
-	}
+    /**
+     * Cancels the task of loading and displaying image for incoming <b>imageAware</b>.
+     *
+     * @param imageAware {@link com.nostra13.universalimageloader.core.imageaware.ImageAware} for which display task
+     *                   will be cancelled
+     */
+    void cancelDisplayTaskFor(ImageAware imageAware)
+    {
+        cacheKeysForImageAwares.remove(imageAware.getId());
+    }
 
-	ReentrantLock getLockForUri(String uri) {
-		ReentrantLock lock = uriLocks.get(uri);
-		if (lock == null) {
-			lock = new ReentrantLock();
-			uriLocks.put(uri, lock);
-		}
-		return lock;
-	}
+    /**
+     * Denies or allows engine to download images from the network.<br /> <br /> If downloads are denied and if image
+     * isn't cached then {@link ImageLoadingListener#onLoadingFailed(String, View, FailReason)} callback will be fired
+     * with {@link FailReason.FailType#NETWORK_DENIED}
+     *
+     * @param denyNetworkDownloads pass <b>true</b> - to deny engine to download images from the network; <b>false</b> -
+     *                             to allow engine to download images from network.
+     */
+    void denyNetworkDownloads(boolean denyNetworkDownloads)
+    {
+        networkDenied.set(denyNetworkDownloads);
+    }
 
-	AtomicBoolean getPause() {
-		return paused;
-	}
+    /**
+     * Sets option whether ImageLoader will use {@link FlushedInputStream} for network downloads to handle <a
+     * href="http://code.google.com/p/android/issues/detail?id=6066">this known problem</a> or not.
+     *
+     * @param handleSlowNetwork pass <b>true</b> - to use {@link FlushedInputStream} for network downloads; <b>false</b>
+     *                          - otherwise.
+     */
+    void handleSlowNetwork(boolean handleSlowNetwork)
+    {
+        slowNetwork.set(handleSlowNetwork);
+    }
 
-	Object getPauseLock() {
-		return pauseLock;
-	}
+    /**
+     * Pauses engine. All new "load&display" tasks won't be executed until ImageLoader is {@link #resume() resumed}.<br
+     * /> Already running tasks are not paused.
+     */
+    void pause()
+    {
+        paused.set(true);
+    }
 
-	boolean isNetworkDenied() {
-		return networkDenied.get();
-	}
+    /**
+     * Resumes engine work. Paused "load&display" tasks will continue its work.
+     */
+    void resume()
+    {
+        paused.set(false);
+        synchronized (pauseLock)
+        {
+            pauseLock.notifyAll();
+        }
+    }
 
-	boolean isSlowNetwork() {
-		return slowNetwork.get();
-	}
+    /**
+     * Stops engine, cancels all running and scheduled display image tasks. Clears internal data.
+     * <br />
+     * <b>NOTE:</b> This method doesn't shutdown
+     * {@linkplain com.nostra13.universalimageloader.core.ImageLoaderConfiguration.Builder#taskExecutor(java.util.concurrent.Executor)
+     * custom task executors} if you set them.
+     */
+    void stop()
+    {
+        if (!configuration.customExecutor)
+        {
+            ((ExecutorService) taskExecutor).shutdownNow();
+        }
+        if (!configuration.customExecutorForCachedImages)
+        {
+            ((ExecutorService) taskExecutorForCachedImages).shutdownNow();
+        }
+
+        cacheKeysForImageAwares.clear();
+        uriLocks.clear();
+    }
+
+    void fireCallback(Runnable r)
+    {
+        taskDistributor.execute(r);
+    }
+
+    ReentrantLock getLockForUri(String uri)
+    {
+        ReentrantLock lock = uriLocks.get(uri);
+        if (lock == null)
+        {
+            lock = new ReentrantLock();
+            uriLocks.put(uri, lock);
+        }
+        return lock;
+    }
+
+    AtomicBoolean getPause()
+    {
+        return paused;
+    }
+
+    Object getPauseLock()
+    {
+        return pauseLock;
+    }
+
+    boolean isNetworkDenied()
+    {
+        return networkDenied.get();
+    }
+
+    boolean isSlowNetwork()
+    {
+        return slowNetwork.get();
+    }
 }
--- library/src/com/nostra13/universalimageloader/utils/ImageServeUtil.java
+++ library/src/com/nostra13/universalimageloader/utils/ImageServeUtil.java
@@ -1,6 +1,8 @@
 package com.nostra13.universalimageloader.utils;
 
 import android.os.Build;
+import com.nostra13.universalimageloader.core.assist.ImageFormat;
+import com.nostra13.universalimageloader.core.assist.ImageServeParams;
 import com.nostra13.universalimageloader.core.assist.ViewScaleType;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.utils.URLEncodedUtils;
@@ -9,6 +11,8 @@ import java.io.UnsupportedEncodingException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
+import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * IntelliJ IDEA.
@@ -22,110 +26,153 @@ public class ImageServeUtil
     static int apiVersion = Build.VERSION.SDK_INT;
 
 
-    public static String getProcessedImageUri(String uri, int width, int height, boolean trasparent, ViewScaleType viewScaleType)
-    {
-        // Check if the path is as expected
-        if (!(uri.contains("blob-key") && uri.contains("imageserve") && uri.contains("http")))
+        public static String getProcessedImageUri(String uri, int width, int height, boolean trasparent, ImageServeParams params, ViewScaleType viewScaleType)
         {
-            return uri;
-        }
+            // Check if the path is as expected
+            if (!(uri.contains("blob-key") && uri.contains("imageserve") && uri.contains("http"))) return uri;
 
-        URI currentUri = null;
-        StringBuilder outUri = new StringBuilder();
+            URI currentUri = null;
+            StringBuilder outUri = new StringBuilder();
 
-        try
-        {
-            currentUri = new URI(uri);
-
-            String authority = currentUri.getAuthority();
-            String path = currentUri.getPath();
-
-            outUri.append("http://");
-            outUri.append(authority);
-            outUri.append(path);
-
-            boolean isFirst = true;
-            for (NameValuePair param : URLEncodedUtils.parse(currentUri, "UTF-8"))
+            try
             {
-                // Ignore fit, fill, thumb and out parameters
-                if (param.getName().equals("fit") || param.getName().equals("fill") || param.getName().equals("out") || param.getName().equals("thumb"))
+                currentUri = new URI(uri);
+
+                String authority = currentUri.getAuthority();
+                String path = currentUri.getPath();
+
+                outUri.append("http://");
+                outUri.append(authority);
+                outUri.append(path);
+
+                boolean isFirst = true;
+                for (NameValuePair param : URLEncodedUtils.parse(currentUri, "UTF-8"))
                 {
-                    continue;
+                    // Ignore fit, fill, thumb and out parameters
+                    if (param.getName().equals("fit") || param.getName().equals("fill") || param.getName().equals("out") || param.getName().equals("thumb")) continue;
+
+
+                    outUri.append(isFirst ? "?" : "&");
+                    isFirst = false;
+
+
+                    outUri.append(URLEncoder.encode(param.getName(), "UTF-8"));
+                    outUri.append("=");
+                    outUri.append(URLEncoder.encode(param.getValue(), "UTF-8"));
                 }
 
-                outUri.append(isFirst ? "?" : "&");
-                isFirst = false;
-                outUri.append(URLEncoder.encode(param.getName(), "UTF-8"));
-                outUri.append("=");
-                outUri.append(URLEncoder.encode(param.getValue(), "UTF-8"));
+                if (viewScaleType != null && width > 0 && height > 0)
+                {
+                    String paramName = null;
+                    String paramValue = null;
+                    if (viewScaleType == ViewScaleType.CROP)
+                    {
+                        //If crop then get fill picture and take smaller edge
+                        paramName = "&fill=";
+                        paramValue = String.valueOf(width > height ? height : width);
+                    }
+                    else if (viewScaleType == ViewScaleType.FIT_INSIDE)
+                    {
+                        //If fill then get fit picture and take larger edge
+                        paramName = "&fit=";
+                        paramValue = String.valueOf(width > height ? width : height);
+                    }
+
+                    outUri.append(paramName);
+                    outUri.append(paramValue);
+                }
+
+                outUri.append("&out=");
+                //If specific format hasnt been requested then calculate format based on transparency and Android API version
+                String format = params != null && params.getImageFormat() != null ? params.getImageFormat().toString() : getImageFormat(trasparent, apiVersion);
+                outUri.append(format);
+
+                if (params != null && params.getBlur() != -1)
+                    addParamToUrl(outUri, "blur", String.valueOf(params.getBlur()));
+
+                if (params != null && params.getQuality() != -1)
+                    addParamToUrl(outUri, "q", String.valueOf(params.getQuality()));
+
+                if (params != null && params.isRecApp())
+                    addParamToUrl(outUri, "recapp", String.valueOf(params.isRecApp()));
+
+            }
+            catch (URISyntaxException e)
+            {
+                return uri;
+            }
+            catch (UnsupportedEncodingException e)
+            {
+                return uri;
             }
 
-            if (viewScaleType != null && width > 0 && height > 0)
-            {
-                String paramName = null;
-                String paramValue = null;
-                if (viewScaleType == ViewScaleType.CROP)
-                {
-                    //If crop then get fill picture and take smaller edge
-                    paramName = "&fill=";
-                    paramValue = String.valueOf(width > height ? height : width);
-                }
-                else if(viewScaleType == ViewScaleType.FIT_INSIDE)
-                {
-                    //If fill then get fit picture and take larger edge
-                    paramName = "&fit=";
-                    paramValue = String.valueOf(width > height ? width : height);
-                }
+            return outUri.toString();
+        }
 
+        private static String getImageFormat(boolean transparent, int versionSDK)
+        {
+            String format;
+            if (transparent)
+            {
+                // Transparent WebP support has appeared in Android 4.2.1+
+                if (versionSDK >= Build.VERSION_CODES.JELLY_BEAN_MR1)
+                {
+                    format = ImageFormat.WEBP.toString();
+                }
+                else
+                {
+                    format = ImageFormat.PNG.toString();
+                }
+            }
+            else
+            {
+                // Non-transparent WebP support has appeared in Android 4.0+
+                if (versionSDK >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+                {
+                    format = ImageFormat.WEBP.toString();
+                }
+                else
+                {
+                    format = ImageFormat.JPEG.toString();
+                }
+            }
+
+            return format;
+        }
+
+        //Parse params from url and return them in map
+        public static Map<String, String> parseImageServeParams(String uri)
+        {
+            Map<String, String> paramsMap = new TreeMap<String, String>();
+
+            int index = uri.indexOf("?");
+            String allParams = uri.substring(index + 1);
+            String[] params = allParams.split("&");
+            for (String param : params)
+            {
+                String[] valueAndName = param.split("=");
+                String valueName = valueAndName[0].equalsIgnoreCase("blob-key") ? "key" : valueAndName[0];
+                paramsMap.put(valueName, valueAndName[1]);
+            }
+
+            return paramsMap;
+        }
+
+        private static void addParamToUrl(StringBuilder outUri, String paramName, String paramValue)
+        {
+            int index = outUri.indexOf(paramName);
+            if (index != -1)
+            {
+                //If paramName is on index then param value is on index + 2 (paramName=paramValue)
+                outUri.replace(index + 2, index + 3, String.valueOf(paramValue));
+            }
+            else
+            {
+                outUri.append("&");
                 outUri.append(paramName);
+                outUri.append("=");
                 outUri.append(paramValue);
             }
-
-            outUri.append("&out=");
-            String format = getImageFormat(trasparent, apiVersion);
-            outUri.append(format);
         }
-        catch (URISyntaxException e)
-        {
-            return uri;
-        }
-        catch (UnsupportedEncodingException e)
-        {
-            return uri;
-        }
-
-        return outUri.toString();
-    }
-
-    private static String getImageFormat(boolean transparent, int versionSDK)
-    {
-        String format;
-        if (transparent)
-        {
-            // Transparent WebP support has appeared in Android 4.2.1+
-            if (versionSDK >= Build.VERSION_CODES.JELLY_BEAN_MR1)
-            {
-                format = "webp";
-            }
-            else
-            {
-                format = "png";
-            }
-        }
-        else
-        {
-            // Non-transparent WebP support has appeared in Android 4.0+
-            if (versionSDK >= Build.VERSION_CODES.ICE_CREAM_SANDWICH)
-            {
-                format = "webp";
-            }
-            else
-            {
-                format = "jpeg";
-            }
-        }
-
-        return format;
-    }
 
 }
